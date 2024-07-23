22ec3bd6537ad4a9c1ab19f7bd173c8807aa84a6
lckturbo <chengkel341@gmail.com>
2024-07-24 03:16:40 +0800

    final updates

--- script.js
+++ script.js
@@ -309,12 +309,17 @@ function currentSlide1(n) {
 function showSlides1(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides1");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex1 = 1}
   if (n < 1) {slideIndex1 = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex1-1].style.display = "block";
+  dots[slideIndex1-1].className += " active";
 }
 
 // Slideshow 2 for Footwork
